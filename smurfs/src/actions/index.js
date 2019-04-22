2f50b7f47904ba96b30b4ce39f782366e3691372
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-22 13:19:37 +0800

    Add update and delete action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -54,3 +54,39 @@ export const addSmurf = smurf => dispatch => {
       dispatch({ type: ADD_DATA_FAILURE, payload: err.response })
     })
 }
+
+export const UPDATE_DATA_START = 'UPDATE_DATA_START'
+export const UPDATE_DATA_SUCCESS = 'UPDATE_DATA_SUCCESS'
+export const UPDATE_DATA_FAILURE = 'UPDATE_DATA_FAILURE'
+
+export const updateSmurf = smurf => dispatch => {
+  dispatch({ type: UPDATE_DATA_START })
+  axios
+    .post(API_ENDPOINT, smurf)
+    .then(res => {
+      console.log(res.data)
+      dispatch({ type: UPDATE_DATA_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      console.log(err.response)
+      dispatch({ type: UPDATE_DATA_FAILURE, payload: err.response })
+    })
+}
+
+export const DELETE_DATA_START = 'DELETE_DATA_START'
+export const DELETE_DATA_SUCCESS = 'DELETE_DATA_SUCCESS'
+export const DELETE_DATA_FAILURE = 'DELETE_DATA_FAILURE'
+
+export const deleteSmurf = id => dispatch => {
+  dispatch({ type: DELETE_DATA_START })
+  axios
+    .delete(`${API_ENDPOINT}/${id}`)
+    .then(res => {
+      console.log(res.data)
+      dispatch({ type: DELETE_DATA_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      console.log(err.response)
+      dispatch({ type: DELETE_DATA_FAILURE, payload: err.response })
+    })
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { FETCH_DATA_START, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, 
-ADD_DATA_START, ADD_DATA_SUCCESS, ADD_DATA_FAILURE
+  ADD_DATA_START, ADD_DATA_SUCCESS, ADD_DATA_FAILURE,
+  UPDATE_DATA_START, UPDATE_DATA_SUCCESS, UPDATE_DATA_FAILURE,
+  DELETE_DATA_START, DELETE_DATA_SUCCESS, DELETE_DATA_FAILURE
 } from '../actions'
 
 const initialState = {
@@ -62,6 +64,58 @@ const reducer = (state = initialState, action) => {
         ...state,
         smurfs: [],
         addingSmurf: false,
+        error: action.payload.data.error,
+        errorStatusCode: action.payload.status,
+      }
+
+    case UPDATE_DATA_START:
+      return {
+        ...state,
+        updatingSmurf: true,
+        error: null,
+        errorStatusCode: null
+      }
+
+    case UPDATE_DATA_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        updatingSmurf: false,
+        error: null,
+        errorStatusCode: null
+      }
+
+    case UPDATE_DATA_FAILURE:
+      return {
+        ...state,
+        smurfs: [],
+        updatingSmurf: false,
+        error: action.payload.data.error,
+        errorStatusCode: action.payload.status,
+      }
+
+    case DELETE_DATA_START:
+      return {
+        ...state,
+        deletingSmurf: true,
+        error: null,
+        errorStatusCode: null
+      }
+
+    case DELETE_DATA_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        deletingSmurf: false,
+        error: null,
+        errorStatusCode: null
+      }
+
+    case DELETE_DATA_FAILURE:
+      return {
+        ...state,
+        smurfs: [],
+        deletingSmurf: false,
         error: action.payload.data.error,
         errorStatusCode: action.payload.status,
       }
