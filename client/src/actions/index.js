fba3cec5ee04626c43bafeb7ef13e85f500018c9
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-22 15:54:59 +0800

    Update endpoint URL

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,29 +1,14 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
-
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
-
 import axios from 'axios'
 
-// const API_ENDPOINT = `${process.env.REACT_APP_API_ENDPOINT || '/'}.netlify/functions/server/api/smurfs`
-const API_ENDPOINT = 'http://localhost:3333/smurfs'
+const API_ENDPOINT = `${process.env.REACT_APP_API_ENDPOINT || '/'}.netlify/functions/server/api/smurfs`
+// const API_ENDPOINT = 'http://localhost:3333/smurfs'
 
 export const FETCH_DATA_START = 'FETCH_DATA_START'
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS'
 export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE'
 
 export const getSmurfs = () => dispatch => {
+  console.log(`API_ENDPOINT: `, API_ENDPOINT)
   dispatch({ type: FETCH_DATA_START })
   axios
     .get(API_ENDPOINT)
